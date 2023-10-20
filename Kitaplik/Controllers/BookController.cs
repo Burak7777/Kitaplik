5ef6027d74b9889ab7a13d0188b2dfeb718300af
Recep İlikçi <Recep liki@DESKTOP-41NAS3H>
2023-10-20 14:29:35 +0300

    Proje dosyası ekle.

---
+++ Kitaplik/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using Kitaplik.Models;
+using Kitaplik.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kitaplik.Controllers;
+
+public class BookController : Controller
+{
+
+    private readonly RepositoryBaglantisi _repository;
+
+    public BookController(RepositoryBaglantisi repository)
+    {
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Creat() 
+    { 
+    return View();
+    }
+
+    [HttpPost]
+    public IActionResult Creat(Book book)
+    {
+        _repository.Add(book);
+        _repository.SaveChanges();
+
+        return RedirectToAction("Index","Home");
+    }
+
+    public IActionResult GetList()
+    {
+        List<Book> books= _repository.Books.ToList();
+        return View(books);
+    }
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        Book book = _repository.Books.Find(id);
+        return View(book);
+    }
+}
